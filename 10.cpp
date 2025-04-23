60f6b493fbae693673c8fecdf3e728f75c9e393c
Kasi-Vishwanathan <coder0110wl@gmail.com>
2025-04-23 16:56:45 +0530

    Delete 10.cpp

--- 10.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int x = 0; // Initialize variable to avoid undefined behavior
-    cout << "Value of x: " << x << endl;
-    return 0;
-}
