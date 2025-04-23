bab83ae4e55f6d211b20689c8962504b75db7d58
Kasi-Vishwanathan <coder0110wl@gmail.com>
2025-04-23 16:55:17 +0530

    Delete 7.cpp

--- 7.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int value = 42;
-    int *ptr = &value; // Assign valid address to pointer
-    cout << "Dereferencing ptr: " << *ptr << endl;
-    return 0;
-}
